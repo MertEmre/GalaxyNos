45867a8cb26d29ccc772349fb5a07256681082c5
MertEmre <32818094+MertEmre@users.noreply.github.com>
2017-10-15 19:06:52 +0300

    Add files via upload

---
+++ OpenNos.Core/Threading/ConcurrentQueueExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+
+namespace OpenNos.Core
+{
+    internal static class ConcurrentQueueExtensions
+    {
+        #region Methods
+
+        public static void Clear<T>(this ConcurrentQueue<T> queue)
+        {
+            while (queue.TryDequeue(out T item))
+            {
+                // do nothing
+            }
+        }
+
+        #endregion
+    }
+}
